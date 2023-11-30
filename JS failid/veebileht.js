748c8511bdb59743e3aa69875079d4967ea486ff
sandisyske <sandraerik9@gmail.com>
2023-11-30 23:58:01 +0200

    pealeht peaaegu valmis

--- JS failid/veebileht.js
+++ JS failid/veebileht.js
@@ -1,1 +1,12 @@
 // click handler ikoonide vajutamiseks
+// Toggle menu click handler võetud https://stackoverflow.com/questions/35579569/hide-show-menu-onclick-javascript
+// see skript muudab vastavalt funtksioonile, kui kaugel äärest asub menüü külg
+// kui on aktiivne showMenu, siis on menüü riba tõmmatud telefoni ekraani peale
+// kui on aktiivne hideMenu siis lükkatakse menüü serv peidetuks parema ääre taha
+var navLinks = document.getElementById("navLinks")
+    function showMenu(){
+        navLinks.style.right = "0"
+    }
+    function hideMenu(){
+        navLinks.style.right = "-200px"
+    } 
